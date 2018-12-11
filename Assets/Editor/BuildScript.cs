8cd8b27aac8124b9bf08ce10c9c7f3ab67852629
GNT-GLOBAL\nam.nh2388 <nam.nh2388@gnt.com.vn>
2018-12-11 14:07:10 +0700

    init

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class BuildScript
+{
+
+    [MenuItem("MyTools/Windows Build With Postprocess")]
+    public static void BuildGame()
+    {
+        // Get filename.
+        string path = "C:/Users/nam.nh2388/desktop/Build";
+        string[] levels = new string[] { "Assets/Scenes/SampleScene.unity" };
+
+        // Build player.
+        BuildPipeline.BuildPlayer(levels, path + "/BuiltGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+
+        // Copy a file from the project folder to the build folder, alongside the built game.
+        //FileUtil.CopyFileOrDirectory("Assets/Templates/Readme.txt", path + "Readme.txt");
+
+        // Run the game (Process class from System.Diagnostics).
+        Process proc = new Process();
+        proc.StartInfo.FileName = path + "/BuiltGame.exe";
+        proc.Start();
+    }
+
+}
